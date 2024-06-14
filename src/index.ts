3f0ae9511078d12f1ee6fc6f75b4a5160ff5cbde
AshGw <AshrefGw@proton.me>
2024-06-15 00:52:55 +0100

    feat: add `ZippedTuple<T>`

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@ type EType<T extends Iterable<unknown>> = T extends Iterable<infer E>
   ? E
   : never;
 
+type ZippedTuple<T extends Array<Iterable<unknown>>> = {
+  [K in keyof T]: EType<T[K]>;
+};
+
 export function* zip<T extends Array<Iterable<unknown>>>(
   ...args: T
-): Generator<{ [K in keyof T]: EType<T[K]> }> {
+): Generator<ZippedTuple<T>> {
   const iterators = args.map((e) => e[Symbol.iterator]());
   while (1) {
     const results = iterators.map((e) => e.next());
     if (results.some(({ done }) => done)) {
       break;
     }
-    yield results.map(({ value }) => value as T) as {
-      [K in keyof T]: EType<T[K]>;
-    };
+    yield results.map(({ value }) => value as T) as ZippedTuple<T>;
   }
 }
